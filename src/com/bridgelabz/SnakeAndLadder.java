ad551200360191c7fe99706b24dcb77e2929fa18
drisyaathul <drisyak809@gmail.com>
2023-04-17 23:43:48 +0800

    Added: Check for Option- No Play,Snake Or Ladder

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -3,8 +3,10 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class SnakeAndLadder {
+
     public static void main(String[] args) {
         System.out.println("*** Welcome to SNAKE AND LADDER PROGRAM ***");
+        System.out.println();
 //UC1
         System.out.println("Lets Start the Game SNAKE AND LADDER");
         int singlePlayerPosition = 0;
@@ -13,5 +15,21 @@ public class SnakeAndLadder {
         Random random = new Random();
         int dice = random.nextInt(6)+1;
         System.out.println("Player rolls the die to get a number Randomly => "+dice);
+//UC3
+        int option = random.nextInt(3)+1;
+        switch (option){
+            case 1:
+                System.out.println("NO PLAY" );
+                System.out.println("The Player stays in the Same Position : "+singlePlayerPosition);
+                break;
+            case 2:
+                System.out.println("LADDER");
+                System.out.println("The Player moves ahead : "+(singlePlayerPosition+dice));
+                break;
+            case 3:
+                System.out.println("SNAKE");
+                System.out.println("The Player moves Behind : " +(singlePlayerPosition-dice));
+                break;
+        }
     }
 }
