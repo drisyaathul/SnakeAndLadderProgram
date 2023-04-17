ce348ba95926f66afb935872545bde8f9d619f79
drisyaathul <drisyak809@gmail.com>
2023-04-18 00:27:23 +0800

    Added: Single Player Reaches The Winning Position

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,32 +4,51 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
+    static final int winningPosition = 100;     //UC4
+
     public static void main(String[] args) {
         System.out.println("*** Welcome to SNAKE AND LADDER PROGRAM ***");
         System.out.println();
 //UC1
-        System.out.println("Lets Start the Game SNAKE AND LADDER");
         int singlePlayerPosition = 0;
-        System.out.println("Single Player Start At Position Zero");
+        System.out.println("Single Player Starts at position = " + singlePlayerPosition);
+        System.out.println();
+//UC4
+        boolean currentTurn = true;
+        while (singlePlayerPosition != winningPosition) {
+            if (currentTurn == true) {
+
 //UC2
-        Random random = new Random();
-        int dice = random.nextInt(6)+1;
-        System.out.println("Player rolls the die to get a number Randomly => "+dice);
+                Random random = new Random();
+                int dice = random.nextInt(6) + 1;
+                System.out.println("Player rolls the die to get a number Randomly => " + dice);
 //UC3
-        int option = random.nextInt(3)+1;
-        switch (option){
-            case 1:
-                System.out.println("NO PLAY" );
-                System.out.println("The Player stays in the Same Position : "+singlePlayerPosition);
-                break;
-            case 2:
-                System.out.println("LADDER");
-                System.out.println("The Player moves ahead : "+(singlePlayerPosition+dice));
-                break;
-            case 3:
-                System.out.println("SNAKE");
-                System.out.println("The Player moves Behind : " +(singlePlayerPosition-dice));
-                break;
+                int option = random.nextInt(3) + 1;
+                switch (option) {
+                    case 1:
+                        System.out.println("NO PLAY");
+                        System.out.println("The Player stays in the Same Position : " + singlePlayerPosition);
+                        System.out.println();
+                        break;
+                    case 2:
+                        System.out.println("LADDER");
+                        System.out.println("The Player moves ahead : " + (singlePlayerPosition + dice));
+                        singlePlayerPosition += dice;
+                        System.out.println();
+                        break;
+                    case 3:
+                        System.out.println("SNAKE");
+                        System.out.println("The Player moves Behind : " + (singlePlayerPosition - dice));
+                        singlePlayerPosition -= dice;
+                        System.out.println();
+                        break;
+                }
+                if (singlePlayerPosition < 0 || singlePlayerPosition > winningPosition) {
+                    singlePlayerPosition = 0;
+                }
+
+            }
         }
+        System.out.println("Single Player Reaches The Winning Position");
     }
 }
