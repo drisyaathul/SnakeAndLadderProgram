3d9bf0584ffe80b4811466e7888525c078fed833
drisyaathul <drisyak809@gmail.com>
2023-04-17 23:06:27 +0800

    Added: Player Rolls a Die to get a Number Randomly

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -5,10 +5,11 @@ import java.util.Random;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         System.out.println("*** Welcome to SNAKE AND LADDER PROGRAM ***");
+//UC1
         System.out.println("Lets Start the Game SNAKE AND LADDER");
         int singlePlayerPosition = 0;
         System.out.println("Single Player Start At Position Zero");
-
+//UC2
         Random random = new Random();
         int dice = random.nextInt(6)+1;
         System.out.println("Player rolls the die to get a number Randomly => "+dice);
