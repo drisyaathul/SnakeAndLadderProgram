3ce71fbc924a94444cbcc4272fdd82b2281e676b
drisyaathul <drisyak809@gmail.com>
2023-04-18 10:49:57 +0800

    Added: Play the Game with 2 Players

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -4,59 +4,104 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
-    static final int winningPosition = 100;     //UC4
-
+    static final int winningPosition = 100;
     public static void main(String[] args) {
         System.out.println("*** Welcome to SNAKE AND LADDER PROGRAM ***");
         System.out.println();
-//UC1
-        int singlePlayerPosition = 0;
-        System.out.println("Single Player Starts at position = " + singlePlayerPosition);
+
+        int firstPlayerPosition = 0;
+        int secondPlayerPosition = 0;
+        System.out.println("Player1 Starts at position = " + firstPlayerPosition);
+        System.out.println("Player2 Starts at position = " + secondPlayerPosition);
         System.out.println();
-        int count = 0;
-//UC4
+        int count1 = 0;
+        int count2 = 0;
+
         boolean currentTurn = true;
-        while (singlePlayerPosition != winningPosition) {
+        while (firstPlayerPosition != winningPosition) {
 
             if (currentTurn == true) {
-//UC2
-                Random random = new Random();
-                int dice = random.nextInt(6) + 1;
-                System.out.println("Player rolls the die to get a number Randomly => " + dice);
-//UC3
-                int option = random.nextInt(3) + 1;
+
+                Random random1 = new Random();
+                int dice = random1.nextInt(6) + 1;
+                System.out.println("Player1 rolls the die to get a number Randomly => " + dice);
+
+                int option = random1.nextInt(3) + 1;
                 switch (option) {
                     case 1:
                         System.out.println("NO PLAY");
-                        System.out.println("The Player stays in the Same Position : " + singlePlayerPosition);
+                        System.out.println("The Player1 stays in the Same Position : " + firstPlayerPosition);
                         System.out.println();
+                        currentTurn = false;
                         break;
                     case 2:
                         System.out.println("LADDER");
-                        System.out.println("The Player moves ahead : " + (singlePlayerPosition + dice));
-                        singlePlayerPosition += dice;
+                        System.out.println("The Player1 moves ahead : " + (firstPlayerPosition + dice));
+                        firstPlayerPosition += dice;
                         System.out.println();
                         break;
                     case 3:
                         System.out.println("SNAKE");
-                        System.out.println("The Player moves Behind : " + (singlePlayerPosition - dice));
-                        singlePlayerPosition -= dice;
+                        System.out.println("The Player1 moves Behind : " + (firstPlayerPosition - dice));
+                        firstPlayerPosition -= dice;
                         System.out.println();
+                        currentTurn = false;
                         break;
                 }
-//UC6
-                count++;
-//UC4
-                if (singlePlayerPosition < 0 ) {
-                    singlePlayerPosition = 0;
+                count1++;
+
+                if (firstPlayerPosition < 0) {
+                    firstPlayerPosition = 0;
                 }
-//UC5
-                if (singlePlayerPosition > winningPosition){
-                   singlePlayerPosition -= dice;
+                if (firstPlayerPosition > winningPosition) {
+                    firstPlayerPosition -= dice;
+                }
+
+            } else {
+                Random random2 = new Random();
+                int dice = random2.nextInt(6) + 1;
+                System.out.println("Player2 rolls the die to get a number Randomly => " + dice);
+
+                int option = random2.nextInt(3) + 1;
+                switch (option) {
+                    case 1:
+                        System.out.println("NO PLAY");
+                        System.out.println("The Player2 stays in the Same Position : " + secondPlayerPosition);
+                        System.out.println();
+                        currentTurn = true;
+                        break;
+                    case 2:
+                        System.out.println("LADDER");
+                        System.out.println("The Player2 moves ahead : " + (secondPlayerPosition + dice));
+                        secondPlayerPosition += dice;
+                        System.out.println();
+                        break;
+                    case 3:
+                        System.out.println("SNAKE");
+                        System.out.println("The Player2 moves Behind : " + (secondPlayerPosition - dice));
+                        secondPlayerPosition -= dice;
+                        System.out.println();
+                        currentTurn = true;
+                        break;
+                }
+                count2++;
+
+                if (secondPlayerPosition < 0) {
+                    secondPlayerPosition = 0;
+                }
+                if (secondPlayerPosition > winningPosition) {
+                    secondPlayerPosition -= dice;
                 }
             }
         }
-        System.out.println(" *** Single Player Reaches the Exact Winning Position *** ");
-        System.out.println("The Number Of times Dice was Played : "+count);
+        if (firstPlayerPosition == winningPosition) {
+            System.out.println(" *** Player1 WON the Game *** ");
+        }else {
+            System.out.println(" *** Player2 WON the Game *** ");
+        }
+//        System.out.println();
+//        System.out.println("Number Of times Player1 rolls the Dice  : " + count1);
+//        System.out.println("Number Of times Player2 rolls the Dice  : " + count2);
     }
 }
+
